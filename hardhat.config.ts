c232fbf2c2cb1e44288abd4048f5c927b235b2ea
jimao <jimao@jimaodeMacBook-Pro.local>
2023-03-21 16:35:28 +0800

    配置修改

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,27 +1,22 @@
-import * as dotenv from "dotenv";
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
 
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+module.exports = {
+  zksolc: {
+    version: "1.3.1",
+    compilerSource: "binary",
+    settings: {},
+  },
+  defaultNetwork: "zkSyncTestnet",
 
-dotenv.config();
-
-const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.9",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+  networks: {
+    zkSyncTestnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
     },
   },
-  networks: {
-    scrollTestnet: {
-      url: process.env.SCROLL_TESTNET_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+  solidity: {
+    version: "0.8.17",
   },
 };
-
-export default config;
